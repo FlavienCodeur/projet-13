1cb57b4ec46285cc179cc5cd5ed2775486cd7554
FlavienCodeur <flavienrousse2003@gmail.com>
2023-01-25 17:54:57 +0100

    push

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 from profiles.views import index, profile
 
@@ -7,4 +9,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('profiles/', index, name='index'),
     path('profiles/<str:username>/', profile, name='profile'),
+    path('sentry-debug/', trigger_error),
+    
 ]
