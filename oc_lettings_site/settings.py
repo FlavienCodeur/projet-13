ba321a9f68ee705320e8466d98c7d794eecbe653
FlavienCodeur <flavienrousse2003@gmail.com>
2023-01-26 09:00:48 +0100

    push

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY' , 'Creer SECRET_KEY dans ton .env')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
